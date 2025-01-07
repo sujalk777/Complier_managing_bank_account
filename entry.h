ceadfa794ec3a327d0538b7c9b13be461f5ddd7d
Sujal Kapoor <2023uee0154@iitjammu.ac.in>
2025-01-07 08:44:47 +0530

    Create entry.h

---
+++ entry.h
@@ -0,0 +1,17 @@
+#include "string"
+#include "ulimitedrational.h"
+using namespace std;
+
+class SymEntry {
+public:
+    string key;
+    UnlimitedRational* val;
+    
+    SymEntry* left;
+    SymEntry* right;
+
+    SymEntry();
+    SymEntry(string k, UnlimitedRational* v);
+
+    ~SymEntry();
+    
